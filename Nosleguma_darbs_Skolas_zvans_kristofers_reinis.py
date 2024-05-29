1236b2f5fce50d2ab72c4673e1f128b45cfc4ded
Kr1st0f3rs <165793796+Kr1st0f3rs@users.noreply.github.com>
2024-05-30 02:46:03 +0300

    Add files via upload

--- Nosleguma_darbs_Skolas_zvans_kristofers_reinis.py
+++ Nosleguma_darbs_Skolas_zvans_kristofers_reinis.py
@@ -1,5 +1,9 @@
 import tkinter as Tk
 from tkinter import *
+from PIL import Image, ImageTk
+from tkinter.ttk import *
+
+
 
 Garums = 700
 Platums = 800
@@ -7,5 +11,10 @@ logs = Tk()
 logs.title("Skolas zvans")
 canva = Canvas(logs, width=Platums, height=Garums, bg="white")
 canva.pack()
+
+#zvans bilde
+image_zvans = Image.open("zvans.png")
+tkimage = ImageTk.PhotoImage(image_zvans)
+image_izvada = canva.create_image(280, 250, image=tkimage)
 
 canva.mainloop()
